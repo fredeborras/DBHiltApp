eff535ebd53f8a59db4a28bd4c757b9388ca4ef3
Frede Borras <frede.borras@gmail.com>
2020-12-01 15:11:06 +0100

    Gradle: Adding Hilt and DataBinding libraries

--- app/src/main/java/com/dbhiltapp/app/ui/main/MainFragment.kt
+++ app/src/main/java/com/dbhiltapp/app/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.dbhiltapp.app.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.dbhiltapp.app.R
 
 class MainFragment : Fragment() {
@@ -23,8 +23,8 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
     }
 
 }
