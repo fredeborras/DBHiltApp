160e14d79e79948a5b1d35d8f34ed2327c404f14
Frede Borras <frede.borras@gmail.com>
2020-12-01 19:22:13 +0100

    Hilt: Creating main ApiModule

---
+++ app/src/main/java/com/dbhiltapp/app/common/rest/ApiModule.kt
@@ -0,0 +1,48 @@
+package com.dbhiltapp.app.common.rest
+
+import com.dbhiltapp.app.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class ApiModule {
+
+    @Provides
+    fun provideBaseUrl() = BuildConfig.BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+    } else {
+        OkHttpClient.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL: String): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiDataSource(retrofit: Retrofit) = retrofit.create(ApiDataSource::class.java)
+
+    @Provides
+    @Singleton
+    fun provideApiRepository(dataSource: ApiDataSource) = ApiRepository(dataSource)
+}
