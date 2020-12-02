9c5c7c3861994ebb43fde26ec67dff2c989991f6
Frede Borras <frede.borras@gmail.com>
2020-12-02 15:37:50 +0100

    Feature - Main: Adding RecyclerView to show items from server

--- app/src/main/java/com/dbhiltapp/app/feature/main/view/MainFragment.kt
+++ app/src/main/java/com/dbhiltapp/app/feature/main/view/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.dbhiltapp.app.feature.main.view
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +8,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.dbhiltapp.app.R
+import com.dbhiltapp.app.databinding.MainFragmentBinding
+import com.dbhiltapp.app.feature.main.entities.PixabayImagesOut
+import com.dbhiltapp.app.feature.main.view.adapter.MainListAdapter
 import com.dbhiltapp.app.feature.main.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.main_fragment.*
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
@@ -19,33 +25,60 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
+
+    private lateinit var loader: ProgressDialog
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        viewModel = ViewModelProvider(this, defaultViewModelProviderFactory).get(MainViewModel::class.java)
+        this.binding = MainFragmentBinding.bind(view)
+        this.viewModel = ViewModelProvider(this, defaultViewModelProviderFactory).get(MainViewModel::class.java)
+
         setupUI()
         setupObservers()
     }
 
     private fun setupUI() {
-        //TODO
+        this.loader = ProgressDialog(context)
+        this.loader.setTitle("Loading...")
+        this.loader.setCancelable(false)
     }
 
     private fun setupObservers() {
-        viewModel.images.observe(viewLifecycleOwner, Observer {
-            //TODO
-            if (it.total!! > 0) {
-
-            } else {
-
-            }
+        this.loader.show()
+        this.viewModel.images.observe(viewLifecycleOwner, Observer {
+            buildPixabayImageList(it)
         })
+    }
+
+    /**
+     * Main function to build list of Pixabay images
+     */
+    private fun buildPixabayImageList(response: PixabayImagesOut) {
+        this.loader.dismiss()
+
+        if (response.hits != null && response.hits!!.isNotEmpty()) {
+            this.binding.listVisible = true
+
+            val adapter = MainListAdapter(response.hits!!)
+            recyclerView.layoutManager = GridLayoutManager(context, 2)
+            recyclerView.adapter = adapter
+        } else {
+            showEmptyState()
+        }
+    }
+
+    /**
+     * Show empty state if there is an error on request or if list of image is empty
+     */
+    private fun showEmptyState() {
+        this.binding.listVisible = false
     }
 
 }
